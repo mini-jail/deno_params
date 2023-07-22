0876b676a25c2713af8889b0ad63a556a2a9a6c9
jail <125803424+mini-jail@users.noreply.github.com>
2023-07-22 19:45:26 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,18 +1,10 @@
 const matcher = /--(\S*)=(?:(?:"([^"]*)")|(?:'([^']*)')|([^\s]+))/img
-const isNumber = /^(\d+)$/
-const isBoolean = /^(true|false)$/
 
 export function getParams(): Record<string, string | number | boolean | undefined> {
   const params = {}
   const regexpMatchArray = Deno.args.join(" ").matchAll(matcher)
   for (const [match, key, v1, v2, v3] of regexpMatchArray) {
-    let value: any = v1 || v2 || v3
-    if (isNumber.test(value)) {
-      value = +value
-    } else if (isBoolean.test(value)) {
-      value = value === "true" ? true : false
-    }
-    params[key] = value
+    params[key] = v1 || v2 || v3
   }
   return params
 }
